0749c5cf7c0cd728f47fc0fa70fc1809407c838b
Asher Glick <asher.glick@gmail.com>
2013-03-02 23:56:05 -0500

    added some scripts to allow for multiple users to control the screen session

---
+++ src/multiUserScripts/shutdown.sh
@@ -0,0 +1,2 @@
+RET=$(echo "\r")
+sudo -u minecraft screen -x minecraft -p 0 -X stuff "stop$RET"
---
+++ src/multiUserScripts/startScreen.sh
@@ -0,0 +1,7 @@
+sudo -u minecraft screen -dmS minecraft -p 0
+sudo -u minecraft screen -x minecraft -p 0 -X multiuser on
+sudo -u minecraft screen -x minecraft -p 0 -X acladd asher
+sudo -u minecraft screen -x minecraft -p 0 -X acladd feygrove
+RET=$(echo "\r")
+sudo -u minecraft screen -x minecraft -p 0 -X stuff "cd /home/minecraft/minecraft$RET"
+sudo -u minecraft screen -x minecraft -p 0 -X stuff "java -Xmx2G -Xms2G -jar craftbukkit.jar nogui;exit$RET"
