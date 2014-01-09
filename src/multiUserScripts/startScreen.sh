dc1968183f3e5f6900c0fc777f78dacc44ab1bbc
Asher Glick <asher.glick@gmail.com>
2014-01-09 09:34:17 -0500

    only allow one server with the same name Added a check to the launch script to see if a server/screen session is allready running

--- src/multiUserScripts/startScreen.sh
+++ src/multiUserScripts/startScreen.sh
@@ -1,7 +1,13 @@
-sudo -u minecraft screen -dmS minecraft -p 0
-sudo -u minecraft screen -x minecraft -p 0 -X multiuser on
-sudo -u minecraft screen -x minecraft -p 0 -X acladd asher
-sudo -u minecraft screen -x minecraft -p 0 -X acladd feygrove
-RET=$(echo "\r")
-sudo -u minecraft screen -x minecraft -p 0 -X stuff "cd /home/minecraft/minecraft$RET"
-sudo -u minecraft screen -x minecraft -p 0 -X stuff "java -Xmx2G -Xms2G -jar craftbukkit.jar nogui;exit$RET"
+#! /bin/sh
+if [ -z $( sudo -u minecraft find /var/run/screen/S-minecraft/ -regex ".*minecraft" ) ]
+then
+	sudo -u minecraft screen -dmS minecraft -p 0
+	sudo -u minecraft screen -x minecraft -p 0 -X multiuser on
+	sudo -u minecraft screen -x minecraft -p 0 -X acladd asher
+	sudo -u minecraft screen -x minecraft -p 0 -X acladd feygrove
+	RET=$(echo "\r")
+	sudo -u minecraft screen -x minecraft -p 0 -X stuff "cd /home/minecraft/minecraft$RET"
+	sudo -u minecraft screen -x minecraft -p 0 -X stuff "java -Xmx2G -Xms2G -jar craftbukkit.jar nogui;exit$RET"
+else
+	echo "There is allready a minecraft server running allready. Stop the server to start another"
+fi
